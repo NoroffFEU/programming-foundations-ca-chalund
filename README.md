37d8f5a7a6fc64f42a2b57de95f4a4708dd73b75
chalund <114383740+chalund@users.noreply.github.com>
2023-01-22 14:56:17 +0100

    upload CA

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=9778863&assignment_repo_type=AssignmentRepo)
 # Programming Foundations CA
+
+# Object is myPet.
+This objet has the following:
+- type: what kind of pet (string),
+- name: name on pet (string),
+- age: how old is pet (number),
+- dangerous: no = false (boolean)
+- bestFriends: name on best friends (array with strings)
+- friendAge: age on friends array with numbers
+- nested objects - detailsOfPet.
+
+# Functions.
+- Find the total age on friends and log number.
+- Build a user interface, circle (ball) with a push button to change color on favorite toy.
+
---
+++ js/myPet.js
@@ -0,0 +1,43 @@
+const myPet = {
+    type: "dog",
+    nameOfPet: "Hjørdis",
+    age: 2,
+    dangerous: false,
+    bestFriends: ["Lexi", "Tassen" , "Rango"],
+    friendsAge: [6, 3, 8],
+
+    favoriteToy: {
+        kind: "ball",
+        color: "purple",
+        makesSound: false,
+    }
+}
+
+console.log(myPet.favoriteToy.color);
+
+
+// Make favourite toy change color
+var button = document.querySelector("button");
+var circle = document.getElementById("changeMe");
+
+function changeColor(){
+    if (circle.style.background == "purple"){
+        circle.style.background = "orange";
+    } else {
+        circle.style.background = "purple";
+    }
+}
+button.onclick = changeColor;
+
+
+
+// Find the total age of pet friends
+var totalAge = myPet.friendsAge;
+
+var sum = totalAge.reduce (function (a, b) {
+    return  a+ b;
+});
+
+console.log(sum);
+
+
